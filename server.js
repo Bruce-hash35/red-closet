af1f5e0f86333bd1961d7a13c5f585018d14ef5f
Bruce-hash35 <draftb135@gmail.com>
2025-12-27 00:46:32 +0300

    Cleaned MongoDB connection for Atlas

--- server.js
+++ server.js
@@ -17,10 +17,8 @@ app.use(session({
 }));
 
 // ✅ Connect to MongoDB Atlas using environment variable
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
+
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
